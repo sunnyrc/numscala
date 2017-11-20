e439d797d405b8d401036ca28e04d2e13121e845
Sanchit Chawla <sanchit_sid@hotmail.com>
2017-11-20 22:34:02 +0700

    Perceptron works!

--- src/main/scala/test/PerceptronTest.scala
+++ src/main/scala/test/PerceptronTest.scala
@@ -5,11 +5,8 @@ import utilities.Utilities
 
 object PerceptronTest extends App{
   val (labels, train) = DigitLoader.load()
-  println("passed load")
-
   // Predict class 9 cos can only perform Binary Classifications
   val predLabel = 9
-
   val y = labels.map(l => if(l == predLabel) 1 else 0)
   val perceptron = new Perceptron()
   perceptron.fit(train, y)
