079aed856590f0c9d9ab5ebc89b7b621e7aa558e
Sanchit Chawla <sanchit_sid@hotmail.com>
2017-11-23 09:01:16 +0700

    working on neural network

--- src/main/scala/nonlinear/NeuralNetwork.scala
+++ src/main/scala/nonlinear/NeuralNetwork.scala
@@ -1,7 +1,7 @@
 package nonlinear
 
 import breeze.generic.{MappingUFunc, UFunc}
-import breeze.linalg.{Axis, DenseMatrix, DenseVector, sum}
+import breeze.linalg.{Axis, DenseMatrix, DenseVector, max, sum}
 import breeze.numerics.{log, pow}
 import breeze.stats.distributions.Rand
 
@@ -66,35 +66,132 @@ class NeuralNetwork(train: DenseMatrix[Double], labels: DenseVector[Int], hidden
     (-y :* log(yPred)) :- ((-y + 1.0) :* log(-yPred + 1.0))
   }
 
-  def h(x: DenseVector[Double], thetas: Array[DenseMatrix[Double]], vecs: List[DenseVector[Double]] = List()): (DenseVector[Double], List[DenseVector[Double]])= {
-    if(thetas.isEmpty) (x(1 to -1), vecs)
+  def h(x: DenseVector[Double], weights: Array[DenseMatrix[Double]], vecs: List[DenseVector[Double]] = List()): (DenseVector[Double], List[DenseVector[Double]])= {
+    if(weights.isEmpty) (x(1 to -1), vecs)
     else {
-      val a = sigmoid(thetas.head * x).toArray
-      h(DenseVector(1.0 +: a), thetas.tail, vecs :+ x(1 to -1))
+      val a = sigmoid(weights.head * x).toArray
+      h(DenseVector(1.0 +: a), weights.tail, vecs :+ x(1 to -1))
     }
   }
 
-  def h(x: DenseMatrix[Double], thetas: Array[DenseMatrix[Double]]): DenseMatrix[Double]= {
-    if(thetas.isEmpty) x(1 to -1, ::).t
+  def h(x: DenseMatrix[Double], weights: Array[DenseMatrix[Double]]): DenseMatrix[Double]= {
+    if(weights.isEmpty) x(1 to -1, ::).t
     else {
       var a: DenseMatrix[Double] =
-        if(thetas.length == weights.length) sigmoid(thetas.head * x.t)
+        if(weights.length == weights.length) sigmoid(weights.head * x.t)
       else {
-        sigmoid(thetas.head * x)
+        sigmoid(weights.head * x)
       }
       a = DenseMatrix.vertcat[Double](DenseMatrix.ones[Double](1, a.cols), a)
-      h(a, thetas.tail)
+      h(a, weights.tail)
     }
+  }
+  def makeZeroMatrix(matrix: Array[DenseMatrix[Double]]): Array[DenseMatrix[Double]] ={
+    matrix.map(m => DenseMatrix.zeros[Double](m.rows, m.cols))
   }
 
   def cost(x: DenseMatrix[Double], thetas: Array[DenseMatrix[Double]], y: DenseMatrix[Double]): Double = {
-    val j = (1.0/x.rows) * sum(
+    val actualCost = (1.0/x.rows) * sum(
       for {i <- 0 until  x.rows - 1
            yPred = h(x(i, ::).inner, thetas)._1
            yi = y(i, ::).inner
       }
         yield sum(loss(yi, yPred)))
-    j
+    actualCost
   }
+
+  def sigmoidGrad(pred: DenseVector[Double]): DenseVector[Double] = pred :* (-pred + 1.0)
+
+  def backpropagate(train: DenseMatrix[Double], xs: DenseMatrix[Double], weights: Array[DenseMatrix[Double]]): Array[DenseMatrix[Double]] = {
+
+    val mat = makeZeroMatrix(weights)
+    mat.foreach(f => f(::,0) := 1.0)
+
+    for (i <- 0 until train.rows){
+      // get actual predictions
+      var (pred, vecs) = h(train(i, ::).inner, weights)
+      var diff = pred - xs(i, ::).inner
+
+      // reverse loop
+      for (j <- weights.length - 1 to 0 by - 1){
+        pred = vecs(j)
+
+        // Formula
+        mat(j)(::, 0) := mat(j)(::,0) :* diff
+        mat(j)(::, 1 to -1) := mat(j)(::, 1 to - 1) + (diff * pred.t)
+
+        diff = (weights(j).t(1 to -1, ::) * diff) :* sigmoidGrad(pred)
+      }
+    }
+    mat.map(_ :/ train.rows.toDouble)
+  }
+
+  def train(learning_rate: Double = 0.1, maxEpoch: Int = 1000) = {
+    var cost = cost(trainPadded, weights, y)
+
+    var epoch = 0
+
+    println(s"Cost at the start: $cost")
+    while (epoch < maxEpoch){
+      val mat = backpropagate(trainPadded, y, weights)
+
+
+      for (l <- 0 until weights.length - 1) {
+        weights(l)(::, 0) := mat(l)(::, 0)
+        weights(l)(::, 1 to -1) := weights(l)(::, 1 to -1) - mat(l)(::, 1 to -1) * learning_rate
+      }
+
+      cost = cost(trainPadded, weights, y)
+      println(s"cost $cost at epoch $epoch")
+
+      if (epoch % 100 == 0){
+        val yPred = predict(train)
+        println(s"Accuracy at epoch $epoch: " + utilities.Utilities.accuracy(labels, DenseVector(yPred:_*)))
+      }
+      epoch+=1
+    }
+
+    def predict(test: DenseMatrix[Double]): List[Int] = {
+      // pad the test
+      val paddedTest = DenseMatrix.horzcat(DenseMatrix.ones(test.rows, 1), test)
+      val yPred = h(paddedTest, weights)
+
+      val predictions =
+        for { i <- 0 until yPred.rows
+        each = yPred(i, ::).inner
+        } yield  each.findAll(_ == max(each)).head
+
+      predictions.map(_ + 1).toList
+    }
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
