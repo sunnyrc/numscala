3069454c1f802cc1df975af6977d595e569ae306
dodinhtrung97 <dodinhtrung97@gmail.com>
2017-12-10 00:09:15 +0700

    Changes to code

--- src/main/scala/test/BigDigit.scala
+++ src/main/scala/test/BigDigit.scala
@@ -8,15 +8,21 @@ import scala.io.Source
 
 object BigDigit {
 
-  def gis(s: InputStream) = new GZIPInputStream(new BufferedInputStream(s))
+    def gis(s: InputStream) = new GZIPInputStream(new BufferedInputStream(s))
 
-  def load(): (DenseVector[Int], DenseMatrix[Double]) = {
-    val f1 = Source.fromInputStream(gis(getClass.getResourceAsStream("digits_train.csv.gz")))
-    val train = f1.getLines().toList.map(_.split(",").map(_.toDouble))
+    /**
+      * Load file
+      * @return
+      */
+    def load(): (DenseVector[Int], DenseMatrix[Double]) = {
+          val f1 = Source.fromInputStream(gis(getClass.getResourceAsStream("digits_train.csv.gz")))
 
-    val f2 = Source.fromInputStream(gis(getClass.getResourceAsStream("digits_labels.csv.gz")))
-    val labels = f2.getLines().toList.map(_.toInt)
+          val f2 = Source.fromInputStream(gis(getClass.getResourceAsStream("digits_labels.csv.gz")))
 
-    (DenseVector(labels:_*), DenseMatrix(train:_*))
-  }
+          val train = f1.getLines().toList.map(_.split(",").map(_.toDouble))
+
+          val labels = f2.getLines().toList.map(_.toInt)
+
+          (DenseVector(labels:_*), DenseMatrix(train:_*))
+      }
 }
--- src/main/scala/test/DigitLoader.scala
+++ src/main/scala/test/DigitLoader.scala
@@ -9,22 +9,37 @@ import scala.io.Source
 
 object DigitLoader {
 
-  val NUM_FEATURES = 64
+    val NUM_FEATURES = 64
 
-  def gis(s: InputStream) = new GZIPInputStream(new BufferedInputStream(s))
+    def gis(s: InputStream) = new GZIPInputStream(new BufferedInputStream(s))
 
-  def getDoubles(vs: Array[String], norm: Boolean = true) =
-    if (norm) {
-      val vsd = vs.map(_.toDouble)
-      val min = vsd.min
-      val maxminDiff = vsd.max - min
-      vsd.map(v => (v - min) / maxminDiff)
+    /**
+      * Map String to double by condition
+      * @param vs
+      * @param norm
+      * @return
+      */
+    def getDoubles(vs: Array[String], norm: Boolean = true) = {
+        if (norm) {
+            val vsd = vs.map(_.toDouble)
+            val min = vsd.min
+            val maxminDiff = vsd.max - min
+
+            vsd.map(v => (v - min) / maxminDiff)
+        } else vs.map(_.toDouble)
     }
-    else vs.map(_.toDouble)
 
-  def load(): (DenseVector[Int], DenseMatrix[Double])  = {
-    val src = Source.fromInputStream(gis(getClass.getResourceAsStream("digits.csv.gz")))
-    val rows = src.getLines().map(_.split(",")).toList.map(row => (row.last.toInt, getDoubles(row.take(NUM_FEATURES))))
+    /**
+      * Load data from file
+      * @return
+      */
+    def load(): (DenseVector[Int], DenseMatrix[Double])  = {
+        val src = Source.fromInputStream(gis(getClass.getResourceAsStream("digits.csv.gz")))
+
+        val rows = src.getLines()
+                        .map(_.split(","))
+                        .toList
+                        .map(row => (row.last.toInt, getDoubles(row.take(NUM_FEATURES))))
 
     (DenseVector(rows.map(_._1):_*), DenseMatrix(rows.map(_._2):_*))
   }
